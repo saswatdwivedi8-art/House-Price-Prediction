d222e67b6c9d1165f0733ea060670c32e9f8a44d
saswatdwivedi8-art <saswatdwivedi8@gmail.com>
2025-12-28 12:28:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,14 @@ scaler = StandardScaler()
 scaled_X = scaler.fit_transform(final_X)
 
 st.sidebar.title('select house features:')
+st.sidebar.image('https://media.tenor.com/Dyg_gZa4Vl4AAAAM/for-sale.gif')
+all_value = []
+for i in final_X:
+  result = st.sidebar.slider(f' select (i) value')
+  all_value.append(result)
+
+
+st.write(all_value)
+
 
 
